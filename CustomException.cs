c4e6cf2d6d28db601580d95bf469d95dd0891241
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-02 14:17:16 +0530

    Added: Custom Exception

---
+++ CustomException.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class CustomException : Exception
+    {
+        public enum ExceptionType
+        {
+            NULL, EMPTY
+        }
+        public ExceptionType type;
+        public CustomException(ExceptionType type, string message) : base(message)
+        {
+            this.type = type;
+        }
+    }
+}
---
+++ MoodAnalyzer.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        private string message;
+
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+        
+
+        public string AnalyzeMood()
+        {
+
+            try
+            {
+                if (this.message.Contains(" "))
+                {
+                    throw new CustomException(CustomException.ExceptionType.EMPTY, "Mood should not be empty");
+                }
+
+                if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomException(CustomException.ExceptionType.NULL, "Mood should not be null");
+            }
+            catch (CustomException exception)
+            {
+                return exception.Message;
+            }
+        }
+    }
+}
