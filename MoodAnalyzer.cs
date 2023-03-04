1497e0ce5e2b570e586092a9a137ac841d0fc594
Nikhlesh Rao <38323670+Nikhlesh117@users.noreply.github.com>
2023-03-04 11:09:54 +0530

    Added: Reflection

---
+++ MoodAnalyzer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MoodAnalyzerProblem
+{
+    public class MoodAnalyzer
+    {
+        private string message;
+
+        public MoodAnalyzer(string message)
+        {
+            this.message = message;
+        }
+
+
+        public string AnalyzeMood()
+        {
+            try
+            {
+                if (this.message.Equals(string.Empty))
+                {
+                    throw new CustomException(CustomException.ExceptionType.EMPTY_MESSAGE, "Mood should not be empty");
+                }
+
+                else if (this.message.Contains("Sad"))
+                {
+                    return "SAD";
+                }
+                else
+                {
+                    return "HAPPY";
+                }
+            }
+            catch (NullReferenceException)
+            {
+                throw new CustomException(CustomException.ExceptionType.NULL_MESSAGE, "Mood should not be null");
+            }
+        }
+    }
+}
